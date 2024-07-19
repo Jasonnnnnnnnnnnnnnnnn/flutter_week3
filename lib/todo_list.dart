92d6bbed893e9fa159322f2fc2cc7e17bd1c4228
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-07-20 01:23:50 +0700

    Todo list itembuilder

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
 
 class TodoList extends StatefulWidget {
-    static const String nameRounte = "/todo_list";
-  const TodoList({Key? key}) : super(key: key);
+  static const String nameRounte = "/todo_list";
+
+  TodoList({Key? key}) : super(key: key);
 
   @override
   _TodoListState createState() => _TodoListState();
 }
 
 class _TodoListState extends State<TodoList> {
+  List<String> _todos = [for (var i = 0; i < 10; i++) 'Todo $i'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Todo List'),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Todo List")
-          ),
-          ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_todos[index]),
+          );
+        },
+        itemCount: _todos.length,
+      ),
     );
   }
 }

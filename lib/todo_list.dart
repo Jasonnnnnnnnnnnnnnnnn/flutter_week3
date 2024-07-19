501cab0e0b3b8d22cbfa9262489f8e5e77319ac5
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-07-20 02:26:58 +0700

    set textfield in controller todolist

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TodoList extends StatefulWidget {
   static const String nameRounte = "/todo_list";
@@ -11,21 +12,55 @@ class TodoList extends StatefulWidget {
 
 class _TodoListState extends State<TodoList> {
   List<String> _todos = [for (var i = 0; i < 10; i++) 'Todo $i'];
+  
+  late TextEditingController _controller;
 
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+@override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todo List'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_todos[index]),
-          );
-        },
-        itemCount: _todos.length,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Todo List'),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Row(
+                
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: EdgeInsets.only(right : 16.0),
+                      child: TextField(
+                        controller:  _controller,
+                      ))),
+                  Expanded(
+                    flex: 1,
+                      child:
+                          ElevatedButton(onPressed: () {}, child: Text('Add')))
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return ListTile(title: Text((_todos[index])));
+                  },
+                  itemCount: _todos.length,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
